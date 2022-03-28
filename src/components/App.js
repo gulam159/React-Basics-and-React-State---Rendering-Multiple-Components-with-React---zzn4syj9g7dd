73ad09d0a5b7bcdc829e4d368ef7861e71cc0401
Gulam Mohiuddeen <90964388+gulam159@users.noreply.github.com>
2022-03-28 14:26:15 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div id="main">
     <h1 data-ns-test="project-name"> My Own Website </h1>
-    <p data-ns-test="project-name"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores deleniti quos id? Quos ipsa nesciunt voluptate! Deleniti aut aliquam quasi vitae laboriosam necessitatibus non eos? </p>
+    <p data-ns-test="project-description"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores deleniti quos id? Quos ipsa nesciunt voluptate! Deleniti aut aliquam quasi vitae laboriosam necessitatibus non eos? </p>
     </div>
   )
 }
